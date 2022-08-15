21a92a1fb4fed8500325940649980cbfb0c393f4
Somavik99 <94855290+Somavik99@users.noreply.github.com>
2022-08-15 11:02:44 +0530

    Add files via upload

--- NewExpenses.js
+++ NewExpenses.js
@@ -2,9 +2,17 @@ import React from "react";
 import "./NewExpenses.css";
 import ExpenseForm from "./ExpenseForm";
 const NewExpenses = () => {
+const saveExpenses = (enteredExpenseData)=>{
+  const expenseData={
+    ...enteredExpenseData,
+    id:Math.random().toString()
+  };
+  console.log(expenseData);
+};
+
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={saveExpenses} />
     </div>
   );
 };
